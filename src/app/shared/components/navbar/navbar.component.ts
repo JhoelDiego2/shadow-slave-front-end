cf48ab087d69cd8e98efe731b1c2138b49e9fa12
JhoelDiego2 <jhoel.mita@sptech.school>
2026-07-28 23:28:30 -0300

    feat: create Navbar component with responsive design and add global styles

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -5,14 +5,7 @@ import { RouterLink } from '@angular/router';
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterLink],
-  template: `
-    <nav class="navbar">
-      <a routerLink="/">Início</a>
-      <a routerLink="/auth/login">Entrar</a>
-      <a routerLink="/auth/register">Registrar</a>
-      <a routerLink="/game">Jogo</a>
-    </nav>
-  `,
+  templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {}
